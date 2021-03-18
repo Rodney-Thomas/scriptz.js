b0f977245fe914cece57d779f9be0022fe46f271
Rodney-Thomas <80913258+Rodney-Thomas@users.noreply.github.com>
2021-03-18 16:39:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var $canvas = document.getElementById('game');
+var canvas = document.getElementById('game');
 
 function scriptz(selector)
 {
   const self =
   {
     center:()=>{
-      self.element.style.margin = "auto"
+      canvas.element.style.margin = "auto"
     }
   }
 }
